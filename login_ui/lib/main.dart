2260e9f32d87a5de8b374c03bd9b0a5194dd1fe8
Murugan K <murugank0204@gmail.com>
2021-08-29 22:32:58 +0530

    scaffold

--- login_ui/lib/main.dart
+++ login_ui/lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Login Page",
+      home: Scaffold(
+        
+      ),
     );
   }
 }
