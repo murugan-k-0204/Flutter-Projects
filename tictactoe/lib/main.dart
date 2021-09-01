edd02228b34df63fcbad6bd7db7505727fb43786
Murugan K <murugank0204@gmail.com>
2021-09-01 21:57:59 +0530

    initial commit for tic tac toe

---
+++ tictactoe/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Tic Tac Toe"),),
+      ),
+    );
+  }
+}
