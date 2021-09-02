8e799532e175efb5f77a2bb36194a3924084ddaf
Murugan K <murugank0204@gmail.com>
2021-09-02 16:34:37 +0530

    tictactoe working fine

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -4,6 +4,10 @@ void main(){
   runApp(HomeScreen());
 }
 
+
+
+
+
 class default_val{
   String a= "";
   String b= "";
@@ -14,6 +18,8 @@ class default_val{
   String g= "";
   String h= "";
   String i= "";
+  String winner = "Play on";
+  bool isDisabled = false;
   
   void init(){
 
@@ -38,9 +44,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late String winner;
   bool color_is_blue = false;
-
+  int announce_winner = 0;
+  late bool is_game_over;
   late String a,b,c,d,e,f,g,h,i;
+  void tttValidator(String a,String b, String c, String d, String e, String f,String g, String h, String i){
+    if((a=='X' && b=='X' && c=='X') || (d=='X' && e=='X' && f=='X') || (g=='X' && h=='X' && i=='X') || (a=='X' && d=='X' && g=='X') || (b=='X' && e=='X' && h=='X') ||(c=='X' && f=='X' && i=='X') ||(a=='X' && e=='X' && i=='X') || (g=='X' && e=='X' && c=='X') ){
+      is_game_over = true;
+      winner = "First player has won";
+    }
+    else if((a=='O' && b=='O' && c=='O') || (d=='O' && e=='O' && f=='O') || (g=='O' && h=='O' && i=='O') || (a=='O' && d=='O' && g=='O') || (b=='O' && e=='O' && h=='O') ||(c=='O' && f=='O' && i=='O') ||(a=='O' && e=='O' && i=='O') || (g=='O' && e=='O' && c=='O') ){
+      is_game_over = true;
+      winner = "Second player has won";
+    }
+    else if( a!="" && b!="" && c!="" && d!="" && e!="" && f!="" && g!="" && h!="" && i!=""){
+      winner = "Oops!! It's a DRAW \n Better luck next time";
+
+    }
+      announce_winner = 0;
+  }
   void start_it(){
     default_val ha = new default_val();
     a = ha.a;
@@ -52,19 +75,21 @@ class _HomeScreenState extends State<HomeScreen> {
     g = ha.g;
     h = ha.h;
     i = ha.i;
+    winner = ha.winner;
+    is_game_over = ha.isDisabled;
   }
-  ElevatedButton singleBox(){
-    return ElevatedButton(
-      onPressed: (){
-        setState(() {
-          color_is_blue = !color_is_blue;
-        });
-      },
-      style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),primary:Colors.white),
-      child: Text(color_is_blue?"":"X", style: TextStyle(fontSize: 30,color: Colors.cyan),),
+  // ElevatedButton singleBox(){
+  //   return ElevatedButton(
+  //     onPressed: (){
+  //       setState(() {
+  //         color_is_blue = !color_is_blue;
+  //       });
+  //     },
+  //     style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),primary:Colors.white),
+  //     child: Text(color_is_blue?"":"X", style: TextStyle(fontSize: 30,color: Colors.cyan),),
       
-    );
-  }
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -88,32 +113,35 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         body: Container(
-          color: Colors.blue,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(winner,style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 30,fontWeight: FontWeight.bold),),
+              Padding(padding: EdgeInsets.only(top: 20)),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children : [
                   ElevatedButton(
+                    child: Text(a,style: TextStyle(fontSize: 40,color:Colors.black,fontWeight: FontWeight.bold),),
+                    style: ElevatedButton.styleFrom(onPrimary: Colors.yellow,shadowColor: Colors.deepOrange,onSurface: Colors.brown,fixedSize: Size(100,100),primary: Colors.white),
                     onPressed: (){
                       setState(() {
-                        if(a==""){
-                          print(b);
+                        if(a=="" && !is_game_over){
                           a=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
-                    style: ElevatedButton.styleFrom(fixedSize: Size(100,100),primary: Colors.white),
-                    child: Text(a,style: TextStyle(fontSize: 40,color:Colors.black,fontWeight: FontWeight.bold),),
+                    
                   ),
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( b== ""){
+                        if( b== "" && !is_game_over){
                           b=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -123,9 +151,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( c== ""){
+                        if( c== "" && !is_game_over){
                           c=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                       
@@ -141,9 +170,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( d== ""){
+                        if( d== "" && !is_game_over){
                           d=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -153,9 +183,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( e== ""){
+                        if( e== "" && !is_game_over){
                           e=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -165,9 +196,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( f== ""){
+                        if( f== "" && !is_game_over){
                           f=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -182,9 +214,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( g== ""){
+                        if( g== "" && !is_game_over){
                           g=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -194,9 +227,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if( h== ""){
+                        if( h== "" && !is_game_over){
                           h=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -206,9 +240,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   ElevatedButton(
                     onPressed: (){
                       setState(() {
-                        if(i == ""){
+                        if(i == "" && !is_game_over){
                           i=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
+                          tttValidator(a, b, c, d, e, f, g, h, i);
                         }
                       });
                     },
@@ -219,6 +254,7 @@ class _HomeScreenState extends State<HomeScreen> {
               )
             ],
           ),
+          
         ),
       ),
     );
