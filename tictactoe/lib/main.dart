66e54fda2474da65bc9ac9076d55e180c7a9b668
Murugan K <murugank0204@gmail.com>
2021-09-02 20:09:52 +0530

    dialog box added after winning

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -1,12 +1,9 @@
+
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(HomeScreen());
+  runApp(MaterialApp(home: HomeScreen()));
 }
-
-
-
-
 
 class default_val{
   String a= "";
@@ -20,23 +17,13 @@ class default_val{
   String i= "";
   String winner = "Play on";
   bool isDisabled = false;
-  
   void init(){
-
   }
 }
 
 bool first_time= true;
-// String a = "";
-// String b = "";
-// String c = "";
-// String d = "";
-// String e = "";
-// String f = "";
-// String g = "";
-// String h = "";
-// String i = "";
 bool to_update_X = true;
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
   @override
@@ -49,21 +36,27 @@ class _HomeScreenState extends State<HomeScreen> {
   int announce_winner = 0;
   late bool is_game_over;
   late String a,b,c,d,e,f,g,h,i;
-  void tttValidator(String a,String b, String c, String d, String e, String f,String g, String h, String i){
+  
+  void tttValidator(String a,String b, String c, String d, String e, String f,String g, String h, String i, BuildContext context){
     if((a=='X' && b=='X' && c=='X') || (d=='X' && e=='X' && f=='X') || (g=='X' && h=='X' && i=='X') || (a=='X' && d=='X' && g=='X') || (b=='X' && e=='X' && h=='X') ||(c=='X' && f=='X' && i=='X') ||(a=='X' && e=='X' && i=='X') || (g=='X' && e=='X' && c=='X') ){
       is_game_over = true;
       winner = "First player has won";
+      if(is_game_over)
+        message(context,winner);
     }
     else if((a=='O' && b=='O' && c=='O') || (d=='O' && e=='O' && f=='O') || (g=='O' && h=='O' && i=='O') || (a=='O' && d=='O' && g=='O') || (b=='O' && e=='O' && h=='O') ||(c=='O' && f=='O' && i=='O') ||(a=='O' && e=='O' && i=='O') || (g=='O' && e=='O' && c=='O') ){
       is_game_over = true;
       winner = "Second player has won";
+      if(is_game_over)
+        message(context,winner);
     }
     else if( a!="" && b!="" && c!="" && d!="" && e!="" && f!="" && g!="" && h!="" && i!=""){
       winner = "Oops!! It's a DRAW \n Better luck next time";
 
     }
-      announce_winner = 0;
+    
   }
+
   void start_it(){
     default_val ha = new default_val();
     a = ha.a;
@@ -78,18 +71,23 @@ class _HomeScreenState extends State<HomeScreen> {
     winner = ha.winner;
     is_game_over = ha.isDisabled;
   }
-  // ElevatedButton singleBox(){
-  //   return ElevatedButton(
-  //     onPressed: (){
-  //       setState(() {
-  //         color_is_blue = !color_is_blue;
-  //       });
-  //     },
-  //     style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),primary:Colors.white),
-  //     child: Text(color_is_blue?"":"X", style: TextStyle(fontSize: 30,color: Colors.cyan),),
-      
-  //   );
-  // }
+
+  void message(BuildContext context, String win){
+    print("do something");
+    var alert = AlertDialog(
+      // title: Text("something working"),
+      content: Text(win,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+      actions: [
+        ElevatedButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: Text("see how he won",style: TextStyle(fontSize: 18),),
+        ),
+      ],
+    );
+    showDialog(context: context, builder: (_) => alert);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -97,8 +95,7 @@ class _HomeScreenState extends State<HomeScreen> {
       first_time = false;
       start_it();
     }
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text("Tic Tac Toe"),
           actions: [
@@ -116,6 +113,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+               // this is new here
               Text(winner,style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 30,fontWeight: FontWeight.bold),),
               Padding(padding: EdgeInsets.only(top: 20)),
               Row(
@@ -129,7 +127,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if(a=="" && !is_game_over){
                           a=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -141,7 +139,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( b== "" && !is_game_over){
                           b=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -154,7 +152,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( c== "" && !is_game_over){
                           c=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                       
@@ -173,7 +171,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( d== "" && !is_game_over){
                           d=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -186,7 +184,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( e== "" && !is_game_over){
                           e=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -199,7 +197,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( f== "" && !is_game_over){
                           f=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -217,7 +215,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( g== "" && !is_game_over){
                           g=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -230,7 +228,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if( h== "" && !is_game_over){
                           h=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -243,7 +241,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if(i == "" && !is_game_over){
                           i=to_update_X?"X":"O";
                           to_update_X = !to_update_X;
-                          tttValidator(a, b, c, d, e, f, g, h, i);
+                          tttValidator(a, b, c, d, e, f, g, h, i,context);
                         }
                       });
                     },
@@ -256,7 +254,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           
         ),
-      ),
-    );
+      );
+  
   }
 }
